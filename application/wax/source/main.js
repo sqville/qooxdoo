d45bc783583b6e61bcf08f6682c0dde9aa6cdcae
sqville <chris.eskew@sqville.com>
2019-01-29 23:19:36 -0600

    Electron testing

--- application/wax/source/main.js
+++ application/wax/source/main.js
@@ -7,7 +7,9 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow()
+
+  mainWindow.maximize()
 
   // and load the index.html of the app.
   mainWindow.loadFile('source/index.html')
