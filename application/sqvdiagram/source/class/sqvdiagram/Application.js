94fbdd71ab4091071d50ef030358e0ae2ce5387d
sqville <chris.eskew@sqville.com>
2019-07-19 13:59:28 -0500

    Application now renders all elements off of a Map object
    Application now renders all elements off of a Map object

--- application/sqvdiagram/source/class/sqvdiagram/Application.js
+++ application/sqvdiagram/source/class/sqvdiagram/Application.js
@@ -19,8 +19,6 @@ qx.Class.define("sqvdiagram.Application",
 {
   extend : qx.application.Standalone,
 
-
-
   /*
   *****************************************************************************
      MEMBERS
@@ -29,9 +27,6 @@ qx.Class.define("sqvdiagram.Application",
 
   members :
   {
-    _docroot : null,
-
-    _lbldetails : null,
     
     /**
      * This method contains the initial application code and gets called 
@@ -57,8 +52,10 @@ qx.Class.define("sqvdiagram.Application",
 
       var desktop = new qx.ui.window.Desktop(windowManager);
 
+      var connobj = new sqvdiagram.Connect();
+
       // Document is the application root
-      var doc = this._docroot = this.getRoot();
+      var doc = this.getRoot();
 
       doc.add(desktop, {edge : 0});
 
@@ -66,14 +63,17 @@ qx.Class.define("sqvdiagram.Application",
       {
         "swimlanes" : [
           {
+            "id" : 1,
             "title" : "Swimlane 01",
             "width" : 1200,
             "height" : 500,
-            "order" : 0
+            "left" : 0,
+            "top" : 0
           }
         ],
         "shapes" : [
           {
+            "id" : 2,
             "title" : "Start",
             "shape" : "circle",
             "width" : 50,
@@ -82,30 +82,34 @@ qx.Class.define("sqvdiagram.Application",
             "top" : 150
           },
           {
+            "id" : 3,
             "title" : "Window 1",
-            "shape" : "default",
+            "shape" : "window",
             "width" : 130,
             "height" : 80,
             "left" : 200,
             "top" : 150
           },
           {
+            "id" : 4,
             "title" : "Window 2",
-            "shape" : "default",
+            "shape" : "window",
             "width" : 130,
             "height" : 80,
             "left" : 620,
             "top" : 170
           },
           {
+            "id" : 5,
             "title" : "Diamond",
-            "shape" : "default",
+            "shape" : "window",
             "width" : 50,
             "height" : 53,
             "left" : 480,
             "top" : 75
           },
           {
+            "id" : 6,
             "title" : "End",
             "shape" : "circle",
             "width" : 50,
@@ -116,47 +120,82 @@ qx.Class.define("sqvdiagram.Application",
         ],
         "connections" : [
           {
-            "elementA" : "",
-            "elementB" : "",
-            "options" : {}
+            "elementA" : 2,
+            "elementB" : 3,
+            "options" : {color : "gray"}
           },
           {
-            "elementA" : "",
-            "elementB" : "",
-            "options" : {}
+            "elementA" : 3,
+            "elementB" : 4,
+            "options" : {color : "green"}
           },
           {
-            "elementA" : "",
-            "elementB" : "",
-            "options" : {}
+            "elementA" : 3,
+            "elementB" : 5,
+            "options" : {color : "blue"}
           },
           {
-            "elementA" : "",
-            "elementB" : "",
-            "options" : {}
+            "elementA" : 5,
+            "elementB" : 6,
+            "options" : {color : "orange"}
+          },
+          {
+            "elementA" : 4,
+            "elementB" : 6,
+            "options" : {color : "yellow"}
           }
         ]
       }
-
-      var winDefs = [
-        [300, 200, 30, 50],
-        [250, 250, 150, 70],
-        [400, 300, 300, 60]
-      ];
-
-      for (var i=0; i<winDefs.length; i++)
+     
+      //Swimlanes
+      for (var i=0; i<diagramdata.swimlanes.length; i++)
       {
-        var def = winDefs[i];
-        var win = new qx.ui.window.Window("Window #" + (i+1)).set({
-          width: def[0],
-          height: def[1],
-          showClose : false,
-          showMinimize : false
+        var def = diagramdata.swimlanes[i];
+        var win = new qx.ui.window.Window(def.title).set({
+          width: def.width,
+          height: def.height,
+          showMaximize : false,
+          showMinimize : false,
+          anonymous: true
         });
-        win.moveTo(def[2], def[3]);
+        win.moveTo(def.left, def.top);
+        desktop.add(win);
+        win.open();
+      }
 
-        //desktop.add(win);
-        //win.open();
+      //Shapes
+      for (var j=0; j<diagramdata.shapes.length; j++)
+      {
+        var defsh = diagramdata.shapes[j];
+        var winsh = new qx.ui.window.Window(defsh.title).set({
+          width: defsh.width,
+          height: defsh.height,
+          showMaximize : false,
+          showMinimize : false,
+          decorator: defsh.shape, 
+          useMoveFrame: true
+        });
+        winsh.setUserData("shapeid", defsh.id);
+        winsh.moveTo(defsh.left, defsh.top);
+
+        // add move listner
+        winsh.addListener("move", function(e) {
+          var allwins = desktop.getWindows();
+          var arrwins = [];
+          allwins.forEach(function(winobj) {
+            if (winobj.getUserData("shapetype")=="connectline") 
+            {
+              if (winobj.getUserData("elementAhashcode") == this.toHashCode() || winobj.getUserData("elementBhashcode") == this.toHashCode())
+              {
+                arrwins.push(winobj);
+              }
+            }
+          }, this);
+          connobj.repositionConnections(arrwins);
+        });
+
+        desktop.add(winsh);
+        winsh.open();
       }
 
       /*
@@ -164,6 +203,7 @@ qx.Class.define("sqvdiagram.Application",
         Below is your sqv diagram code...
       -------------------------------------------------------------------------
       */
+     /*
       // Swimlanes
       var swimlane01 = new qx.ui.window.Window("Swimlane 01");
       swimlane01.set({ width: 1200, height: 500, showMaximize: false, showMinimize: false, anonymous: true });
@@ -199,10 +239,12 @@ qx.Class.define("sqvdiagram.Application",
       win2.open();
       win3.open();
       winend.open();  
+      */
 
       // draw connectors on appear of the app
       desktop.addListener("appear", function(e) {
         // Add move listeners to the windows
+        /*
         var allshapes = desktop.getWindows();
         allshapes.forEach(function(shape) {
           shape.addListener("move", function(e) {
@@ -220,12 +262,30 @@ qx.Class.define("sqvdiagram.Application",
             connobj.repositionConnections(arrwins);
           });
         });
-        var connobj = new sqvdiagram.Connect();
+        */
+        
+        var alldsktpwins = desktop.getWindows();
+
+        //Connections
+        for (var k=0; k<diagramdata.connections.length; k++)
+        {
+          var defc = diagramdata.connections[k];
+          var eleA = alldsktpwins.find(function(elA) { 
+            return elA.getUserData("shapeid") == defc.elementA;
+          });
+          var eleB = alldsktpwins.find(function(elB) {
+            return elB.getUserData("shapeid") == defc.elementB;
+          });
+          connobj.connect(eleA, eleB, defc.options, desktop);
+        }
+
+        /*
         connobj.connect(winstart, win1, {color : "gray"}, desktop);
         connobj.connect(win1, win2, {color : "gray"}, desktop);
         connobj.connect(win1, win3, {color : "gray"}, desktop);
         connobj.connect(win3, winend, {color : "gray"}, desktop);
         connobj.connect(win2, winend, {color : "gray"}, desktop);
+        */
       });
 
     }
